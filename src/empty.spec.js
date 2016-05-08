b67c44421312cf3230f8c907271a925a3ba9fc6e
Robert Smith <rbrtsmith84@googlemail.com>
2016-05-08 22:56:01 +0100

    test

---
+++ src/empty.spec.js
@@ -0,0 +1,29 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+
+const CoolComponent = ({ greeting }) => (
+  <div>
+    <h1>Greeting</h1>
+    <div>
+      {greeting}!
+    </div>
+  </div>
+);
+
+describe('<CoolComponent />', () => {
+  it('should render the greeting', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<CoolComponent greeting='hello world' />);
+    const actual = renderer.getRenderOutput();
+    const expected = (
+      <div>
+        <h1>Greeting</h1>
+        <div>hello world</div>
+      </div>
+    );
+    expect(actual).toEqualJSX(expected);
+  });
+});
